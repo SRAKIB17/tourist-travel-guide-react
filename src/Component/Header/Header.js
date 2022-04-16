f39b661222d15461cb0b528be769ba9058a4d14b
SRAKIB17 <rakibulssc5@gmail.com>
2022-04-16 23:43:06 +0600

    header ok

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/home"><img src={logo} alt="" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/categories">Categories</Nav.Link>
+                        
+                        <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
