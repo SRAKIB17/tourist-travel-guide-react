53e4de4de55d97f01640fc47c30ffa27931107ce
SRAKIB17 <rakibulssc5@gmail.com>
2022-04-17 20:54:43 +0600

    login page ok

--- src/Component/Pages/Login/Login.js
+++ src/Component/Pages/Login/Login.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import { useAuthState, useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import React, { useRef, useState } from 'react';
+import { useAuthState, useCreateUserWithEmailAndPassword, useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Google } from 'react-bootstrap-icons';
+import { Github, Google } from 'react-bootstrap-icons';
 import Loading from '../Loading/Loading';
 
 
 const Login = () => {
     const loaction = useLocation()
     const from = loaction.state?.from?.pathname || '/'
-    console.log(from)
+
+
     const [register, setRegister] = useState(true)
     const [target, setTarget] = useState(true)
+    const [resetTarget, setResetTarget] = useState(false)
     const navigate = useNavigate()
     const emailHandlar = (event) => {
         const check = /\S+@\S+\.\S+/.test(event.target.value)
@@ -26,77 +28,112 @@ const Login = () => {
         }
     }
 
-    let createUserWithEmailAndPassword;
-    let user;
-    let loading;
-    let error;
-    let signInWithEmailAndPassword;
-    let signInWithGoogle;
 
-    [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
-    [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+    const [createUserWithEmailAndPassword, user1, loading1, error1] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
-    [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth)
-    // form handle 
+    const [signInWithEmailAndPassword, user2, loading2, error2] = useSignInWithEmailAndPassword(auth);
+
+    const [signInWithGoogle, user3, loading3, error3] = useSignInWithGoogle(auth)
+
+    const [signInWithGithub, user4, loading4, error4] = useSignInWithGithub(auth);
+    console.log(error4)
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+    // form handle
+    const emailRef = useRef('')
     const formSubmitHandler = event => {
-        const email = event.target.email.value;  
+        const email = event.target.email.value;
         const password = event.target.password.value;
         event.preventDefault()
-        console.log(register)
+        
         if (!register) {
             createUserWithEmailAndPassword(email, password)
 
         }
         else {
             signInWithEmailAndPassword(email, password)
+            
         }
     }
-    [user, loading] = useAuthState(auth)
+    const [user, loading] = useAuthState(auth)
     if (loading) {
         return <Loading></Loading>
     }
-    if (user) {
+    let errMsg = ''
+    if((error||error1)||(error2||error3)||error4){
+        errMsg = error?.message || error1?.message || error2?.message ||error3?.message ||error4?.message
+    }
+    if (user || user1|| user2 || user3 || user4) {
         navigate(from)
     }
 
     // for sign in other option 
     let option
-    if(register){
+    if (register) {
         option = 'Login';
     }
-    else{
+    else {
         option = 'Sign up';
+    }
+    const resetPass = (event) => {
+        const email = emailRef.current.value;
+        sendPasswordResetEmail(email)
+        
     }
     return (
         <div className='login'>
-            <h1>Please {register ? 'Login' : 'Sign Up'}</h1>
-            <p>{error && error.message}</p>
+            {
+                resetTarget ? <h1>Reset Password</h1> : <h1>Please {register ? 'Login' : 'Sign Up'}</h1>
+            }
+            <p>{errMsg===''|| errMsg}</p>
             <form onSubmit={formSubmitHandler}>
                 {
                     register || <input type="text" name="name" id="" placeholder='Name' />
                 }
-                <input className='' type="email" name="email" id="" placeholder='Email' onChange={emailHandlar} />
-                <input type="password" name="password" id="" placeholder='Password' />
-
-                <input disabled={target} type="submit" value={register ? 'Login' : 'Sign Up'} />
-                <p className='h5 mx-4'>
-                    Forget password {register&& <button className='btn' style={{fontSize:'20px'}}>Reset Password</button>}
-                </p>
+                <input className='' ref={emailRef} type="email" name="email" id="" placeholder='Email' onChange={emailHandlar} />
+                {
+                    resetTarget ||
+                        <>
+                            <input type="password" name="password" id="" placeholder='Password' />
+                            <input className='btn' disabled={target} type="submit" value={register ? 'Login' : 'Sign Up'} />
+                        </>
+                }
             </form>
-            
+
+
+            {/* reset button  */}
+            {
+                resetTarget&&<button disabled={target} style={{ width: '400px', fontSize: '25px' }} className='btn btn-warning m-1 text-light' onClick={resetPass}>Reset Password</button>
+            }
+
+            {/* forget password  */}
+            {resetTarget || register && <p className='h5 mx-4'>Forget password <button className='btn' style={{ fontSize: '20px' }} onClick={() => {
+                setResetTarget(!resetTarget)
+                errMsg = ''
+                }}>Reset Password</button></p>}
+
+            {/* or  */}
             <div className='LoginOr'>
                 <div></div>
                 <p>Or</p>
                 <div></div>
             </div>
+
             {/* for google /github */}
-            <div>
-                <button style={{width:'400px',fontSize:'25px'}} className='btn btn-info' onClick={()=>signInWithGoogle()}><Google/> {option} with Google</button>
-            </div>
-            
+            {resetTarget || <div>
+                <button style={{ width: '400px', fontSize: '25px' }} className='btn btn-info m-1 text-light' onClick={() => signInWithGoogle()}><Google /> {option} with Google</button>
+                <br />
+                <button style={{ width: '400px', fontSize: '25px' }} className='btn btn-warning m-1 text-light' onClick={() => signInWithGithub()}><Github />{option} with GitHub</button>
+            </div>}
+
+            {/* register  */}
             <div className='d-flex align-items-center'>
-                <p className='h5'>{register ? 'New User? ' : 'Already have an account'}<button className='btn' style={{ fontSize: '25px' }} onClick={() => setRegister(!register)}>{register ? 'Create a new account' : 'Login'}</button></p>
+                <p className='h5'>{register ? 'New User? ' : 'Already have an account'}<button className='btn ' style={{ fontSize: '25px' }} onClick={() => {
+                    setRegister(!register)
+                    setResetTarget(false)
+                    errMsg = ''
+                    
+                }}>{register ? 'Create a new account' : 'Login'}</button></p>
             </div>
         </div>
     );
